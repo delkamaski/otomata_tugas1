831b2199afc5f0831fa37850d08ac22760bc53aa
Indhira Ayu <162036447+indhiraya@users.noreply.github.com>
2025-03-02 15:04:17 +0700

    Create BinaryChecker.cpp

---
+++ BinaryChecker.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> patterns = {"00", "10", "010", "01001"};
+
+bool validate(const string& input, int start) {
+    if (start == input.size()) return true; 
+    for (const string& pattern : patterns) {
+        if (input.substr(start, pattern.size()) == pattern) {
+            if (validate(input, start + pattern.size())) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool check(string testcase) {
+    return validate(testcase, 0) || validate(string(testcase.rbegin(), testcase.rend()), 0);
+}
+
+int main() {
+    string testcase;
+    cout << "Masukkan bit string: ";
+    cin >> testcase;
+    cout << testcase << ": " << (check(testcase) ? "Valid" : "Tidak Valid") << endl;
+    
+    return 0;
+}
